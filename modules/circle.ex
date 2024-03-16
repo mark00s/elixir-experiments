50596843ca3911e770a05545db0b0cdd901a4708
Marek Jakóbiak <marek.jakobiak@gmail.com>
2024-03-16 14:45:18 +0100

    doc: Added @spec to Circle

--- modules/circle.ex
+++ modules/circle.ex
@@ -7,10 +7,12 @@ defmodule Circle do
   @doc """
   Computes area of a circle
   """
+  @spec area(number) :: number
   def area(r), do: r * r * @pi
 
   @doc """
   Computes the circumference of a circle
   """
+  @spec circumference(number) :: number
   def circumference(r), do: 2 * r * @pi
 end
